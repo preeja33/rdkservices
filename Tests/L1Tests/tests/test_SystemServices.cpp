5abbd1dc34ff9e575b5ef7a70f486534bf69748e
preeja33 <preeja_raveendran@comcast.com>
2025-09-12 19:38:26 +0530

    Update test_SystemServices.cpp

--- Tests/L1Tests/tests/test_SystemServices.cpp
+++ Tests/L1Tests/tests/test_SystemServices.cpp
@@ -34,7 +34,7 @@
 #include "exception.hpp"
 
 #include <fstream>
-
+#define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
 using namespace WPEFramework;
 
 using ::testing::NiceMock;
@@ -186,6 +186,7 @@ protected:
         ASSERT_TRUE(powerEventHandler != nullptr);
     }
 };
+
 TEST_F(SystemServicesTest, InvalidTerritory)
 {
      
