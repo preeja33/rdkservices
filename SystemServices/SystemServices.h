7de24392db0e2410815ef1dd2619dea9275107d1
preeja33 <preeja_raveendran@comcast.com>
2025-09-15 17:25:56 +0530

    Update SystemServices.h

--- SystemServices/SystemServices.h
+++ SystemServices/SystemServices.h
@@ -186,6 +186,8 @@ namespace WPEFramework {
 		std::string getStbBranchString();
                 bool makePersistentDir();
 		std::string safeExtractAfterColon(const std::string& inputLine);
+        void threadWriter();
+        void threadReader();
 
 #if defined(USE_IARMBUS) || defined(USE_IARM_BUS)
                 void InitializeIARM();
