f2fca4697f2e815b8f54c526ef8a06c33dd35047
preeja33 <preeja_raveendran@comcast.com>
2025-09-16 14:51:47 +0530

    Update SystemServices.cpp

--- SystemServices/SystemServices.cpp
+++ SystemServices/SystemServices.cpp
@@ -457,8 +457,8 @@ namespace WPEFramework {
             registerMethod("setNetworkStandbyMode", &SystemServices::setNetworkStandbyMode, this);
             registerMethod("getNetworkStandbyMode", &SystemServices::getNetworkStandbyMode, this);
             registerMethod("getPowerStateIsManagedByDevice", &SystemServices::getPowerStateIsManagedByDevice, this);
-    	     registerMethod("setTerritory", &SystemServices::setterritory, this);
-	         registerMethod("getTerritory", &SystemServices::getterritory, this);
+    	     registerMethod("setTerritory", &SystemServices::setTerritory, this);
+	         registerMethod("getTerritory", &SystemServices::getTerritory, this);
 			registerMethod("setWakeupSrcConfiguration", &SystemServices::setWakeupSrcConfiguration, this);
 	    registerMethod("getWakeupSrcConfiguration", &SystemServices::getWakeupSrcConfiguration, this);
 
